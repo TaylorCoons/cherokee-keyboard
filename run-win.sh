af8eec10618056ae3686ce8d984ec3cdaf4f2163
Taylor Coons <tcoons@mymail.mines.edu>
2019-12-07 18:43:01 -0700

    Updated run-win script

--- run-win.sh
+++ run-win.sh
@@ -1,1 +1,4 @@
 g++ main.cpp KeyEmulator.h KeyEmulator.cpp -o cherokee-keyboard.exe -I"C:\SFML-2.5.1\include" -L"C:\SFML-2.5.1\lib" -lsfml-window -lsfml-system -lsfml-graphics
+cp cherokee-keyboard.exe exe/
+cd exe/
+./cherokee-keyboard.exe
