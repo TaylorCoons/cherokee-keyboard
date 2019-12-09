0b9eabe5d4fe91ba8fae284b4f59937d7dc769ed
Taylor Coons <TaylorCoons8798@gmail.com>
2019-12-08 17:40:46 -0700

    Hooked and send msgs

--- KeyEmulator.cpp
+++ KeyEmulator.cpp
@@ -1,32 +1,39 @@
 #include "KeyEmulator.h"
 
-// Check for which system
-#if defined(__WIN32) || defined(__WIN64)
-#define WIN_SYS
-#elif defined(__linux__) || defined(__linux) || defined(linux)
-#define LINUX_SYS
-#endif
+LRESULT __stdcall HookCallbackKeyboard(int nCode, WPARAM wParam, LPARAM lParam) {
+  if (nCode >= 0) {
+    KBDLLHOOKSTRUCT kbdStruct;
+    kbdStruct = *((KBDLLHOOKSTRUCT*)lParam);
+    if(wParam == WM_KEYDOWN) {
+      int chChar = KeyEmulator::Translate(kbdStruct.vkCode);
+      if (chChar) {
+        PostMessageW(HWND_BROADCAST, WM_CHAR, 0x13A1, 1L);
+        return -1;
+      }
+    }
+  }
+  return CallNextHookEx(NULL, nCode, wParam, lParam);
+}
 
 KeyEmulator::KeyEmulator() {
-    enable = true;
+  enable = true;
+  kbHook = SetWindowsHookEx(WH_KEYBOARD_LL, HookCallbackKeyboard, NULL, 0);
+  if (!kbHook) {
+    std::cout << "Failed to link keyboard" << std::endl;
+  }
 }
 
 void KeyEmulator::Enable(bool enable) {
-    this->enable = enable;
+  this->enable = enable;
 }
 
-#ifdef WIN_SYS
-void KeyEmulator::Run() {
-
+int KeyEmulator::Translate(int vkCode) {
+  int c = (int)MapVirtualKey(vkCode, 2);
+  switch (c) {
+    case
+  }
 }
-#endif
-
-#ifdef LINUX_SYS
-void KeyEmulator::Run() {
-
-}
-#endif
-
 
 KeyEmulator::~KeyEmulator() {
+  UnhookWindowsHookEx(kbHook);
 }
--- KeyEmulator.h
+++ KeyEmulator.h
@@ -3,25 +3,27 @@
 
 #include <iostream>
 #include <stdlib.h>
-#include <regex>
-#include <string> 
-class KeyEmulator {
+#include <string>
+#include <Windows.h>
 
-    /* Variables */
-    private:
-    // Enabled / Disabled
-    bool enable;
-    
-    /* Functions */  
-    public:
-    // CTOR
-    KeyEmulator();
-    // Enable
-    void Enable(bool enable);
-    // Run emulator
-    void Run();
-    // DTOR
-    ~KeyEmulator();
+class KeyEmulator {
+  /* Variables */
+  private:
+  // Enabled / Disabled
+  bool enable;
+  HHOOK kbHook;
+  /* Functions */
+  public:
+  // CTOR
+  KeyEmulator();
+  // Enable
+  void Enable(bool enable);
+  // Translate vk key to cherokee character
+  static int Translate(int vkCode);
+  // DTOR
+  ~KeyEmulator();
 };
+
+extern KeyEmulator keyEm;
 
 #endif
